0a2a06caba5edab7a9a65042a623fff60f687872
Dinh · divn.dev <me@divn.dev>
2023-09-21 11:00:36 +0700

    init

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import robotsTxt from 'astro-robots-txt'
+import { defineConfig } from 'astro/config'
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://divn.dev',
+  integrations: [mdx(), sitemap(), robotsTxt()],
+})
